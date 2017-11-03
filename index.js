8a0de11c1fdc96146965311d8ad0193916e54bcf
Hana Shiro <shirohana0608@gmail.com>
2017-11-03 23:10:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const dev = process.env.NODE_ENV !== 'production'
+const production = process.env.NODE_ENV === 'production'
 
-process.env.DEBUG = 'api:*'
+if (!production) {
+  process.env.DEBUG = 'api:*'
+}
+
 const port = process.env.PORT || 4000
 
 const express = require('express')
@@ -13,15 +16,20 @@ const lru = require('lru-cache')
 
 // Clean up
 rimraf.sync('i18n')
+cpx.copySync('locales/*.json', 'i18n')
 
-runCopy() // Copy files to prevent translation lost after JSON.stringify
-runStylus() // Build global style `style/stylus/index.styl > dist/style.css`
-// runBulma() // Build customized Bulma
+if (!production) {
+  watchLocales() // Copy files to prevent translation lost after JSON.stringify
+  watchStylus() // Build global style `style/stylus/index.styl > dist/style.css`
+  // watchBulma() // Build customized Bulma
+}
 
 const locales = glob.sync('*.json', { cwd: 'locales' }).map(v => v.slice(0, -5))
 const app = express()
 
 app.disable('x-powered-by')
+
+app.set('trust proxy', 'loopback')
 
 app.set('views', 'views')
 app.set('view engine', 'pug')
@@ -37,7 +45,7 @@ i18n.expressBind(app, {
   locales,
   directory: 'i18n',
   extension: '.json',
-  devMode: dev
+  devMode: !production
 })
 
 app.use('/', dynapi({
@@ -51,30 +59,16 @@ app.listen(port, () => {
   console.log(`Server starts on http://127.0.0.1:${port}`)
 })
 
-function runCopy () {
-  if (dev) {
-    cpx.watch('locales/*.json', 'i18n', { clean: true, initialCopy: true })
-  } else {
-    cpx.copySync('locales/*.json', 'i18n')
-  }
+function watchLocales () {
+  cpx.watch('locales/*.json', 'i18n', { initialCopy: false })
 }
 
-function runStylus () {
-  const args = 'style/stylus/index.styl -o dist/main.css --sourcemap'.split(' ')
-  if (dev) {
-    args.push('-w')
-  }
-  const proc = require('child_process').spawn('stylus', args, {
-    stdio: 'inherit'
-  })
+function watchStylus () {
+  const args = 'style/stylus/index.styl -o dist/main.css --sourcemap -w'.split(' ')
+  const proc = require('child_process').spawn('stylus', args, { stdio: 'inherit' })
 }
 
-function runBulma () {
-  const args = 'style/bulma.sass dist/bulma.css --source-map dist/bulma.css.map'.split(' ')
-  if (dev) {
-    args.push('-w')
-  }
-  const proc = require('child_process').spawn('node-sass', args, {
-    stdio: 'inherit'
-  })
+function watchBulma () {
+  const args = 'style/bulma.sass dist/bulma.css --source-map dist/bulma.css.map -w'.split(' ')
+  const proc = require('child_process').spawn('node-sass', args, { stdio: 'inherit' })
 }
